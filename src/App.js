afd7d8db5aedb23aa7dd7af6267599c4d17b002d
Ahmad Hassaan <ahmedoppal2592@gmail.com>
2025-04-03 23:20:38 -0700

    useEffect Hook

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
-//import logo from './logo.svg';
 import './App.css';
-import HookCountertwo from './Components/HookCountertwo';
-//import HookCounter from './Components/HookCounter';
+//import HookCountertwo from './Components/HookCountertwo';
+import HooksEffectCounter from './Components/HooksEffectCounter';
 
 function App() {
   return (
     <div className="App">
-      {/* <HookCounter /> */}
-      <HookCountertwo />
+      <HooksEffectCounter />
     </div>
   );
 }
---
+++ src/Components/HooksEffectCounter.js
@@ -0,0 +1,16 @@
+import React,{useState, useEffect} from 'react'
+
+function HooksEffectCounter() {
+    const [count,setCount] = useState(0)
+
+    useEffect(() => {
+        document.title = `Clicked ${count} times`
+    })
+  return (
+    <div>
+        <button onClick={() => setCount(count + 1)}>clicked {count} times</button>
+    </div>
+  )
+}
+
+export default HooksEffectCounter
